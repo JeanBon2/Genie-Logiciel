d5d9ca00ad3b8d4c370e341a01ac9d017a5951c3
DUC <Anne-SophieDuc@hotmail.fr>
2018-03-06 08:11:14 +0100

    Merge

---
+++ DiseaseForecaster/TAnalyse.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "c:\Users\asduc.INSA-LYON\Documents\Génie logiciel\Genie-Logiciel\DiseaseForecaster\tests\AbstractTest.h"
+class TAnalyse :
+	public AbstractTest
+{
+public:
+	TAnalyse();
+	~TAnalyse();
+	void AnalyseTest();
+	void displayContentTest();
+	void displayHeaderTest();
+};
+
