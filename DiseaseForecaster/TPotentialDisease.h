e8fe21c71f7f962644f69ff4a84fbc8420799bf2
DUC <Anne-SophieDuc@hotmail.fr>
2018-03-14 13:02:09 +0100

    Revert "Tests unit"
    This reverts commit 9a7aa518a3a921645996b4190c1bfc293709520c.

--- DiseaseForecaster/TPotentialDisease.h
+++ DiseaseForecaster/TPotentialDisease.h
@@ -7,8 +7,8 @@
 using namespace std;
 
 // Personnal include
-#include "tests/AbstractTest.h"
-#include "tests/MethodTest.h"
+#include "c:\Users\asduc.INSA-LYON\Documents\Génie logiciel\Genie-Logiciel\DiseaseForecaster\tests\AbstractTest.h"
+#include "c:\Users\asduc.INSA-LYON\Documents\Génie logiciel\Genie-Logiciel\DiseaseForecaster\tests\MethodTest.h"
 #include "PotentialDisease.h"
 
 // Constants
