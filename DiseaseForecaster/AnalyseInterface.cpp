075e0400ba03f7707f5b2e47eef9b7e2cb4f0c03
Antoine Mathat <antoine.mathat@yahoo.fr>
2018-03-16 15:31:28 +0100

    AnalyseInterface push

--- DiseaseForecaster/AnalyseInterface.cpp
+++ DiseaseForecaster/AnalyseInterface.cpp
@@ -1,14 +1,15 @@
 // System include
 #include <iostream>
+#include <QFile>
+#include <typeinfo>
+#include <string>
 using namespace std;
 
 // Personnal include
 #include "AnalyseInterface.h"
 #include "CsvParser.h"
-#include <QFile>
-#include <typeinfo>
-#include <string>
-#include <stdlib.h>
+#include "ContinuousAttribute.h"
+#include "DiscreteAttribute.h"
 
 // Constants
 
@@ -24,30 +25,61 @@ void AnalyseInterface::run()
 
 vector<Analyse> AnalyseInterface::AnalysePrintFile(string filePath)
 {
+
+
   vector<Analyse> analyses;
+
   vector<HealthPrint*> healthPrints = getHealthPrintFromFile(filePath);
 
-  cout << "==========================" << endl;
+/*  cout << "==========================" << endl;
   for (auto&& it :healthPrints)
   {
     it->displayContent();
     cout << endl;
   }
+  */
 
   // 2. Get all Diseases
+  vector<Disease> diseases = getAllDiseases();
+  vector<ContinuousAttribute> continuousAttributes;
+  vector<DiscreteAttribute> discreteAttributes;
+
 
   // 3. Analyse
-
-  /*
-  for (auto&& it :healthPrints)
+  for (auto&& hp : healthPrints)
   {
-    for (auto&& it2 : it)
+    vector<PotentialDisease> potencialDiseases;
+
+    for (auto&& dis : diseases)
     {
-      cout << "avant" << endl;
-      cout << it2 << endl;
-      cout << "après" << endl;
+      double nbAnormalAttributes = 0;
+      double nbAttributes = 0;
+
+      for (auto&& hpAttribute : hp->getContinuousAttributesValues())
+      {
+        nbAttributes++;
+      }
+      for (auto&& hpAttribute : hp->getDiscreteAttributesValues())
+      {
+        dis->getDiscriminantAttributes();
+        nbAttributes++;
+      }
+
+      if (nbAttributes / nbAnormalAttributes > 0.8)
+      {
+        PotentialDisease pd();
+        potencialDiseases.push_back()
+      }
     }
-  }*/
+
+    //Analyse analyse;
+  }
+
+
+
+  // 4. INSERTION DES RÉSULTATS
+
+  
     return analyses;
 }
 
@@ -61,14 +93,19 @@ vector<Analyse> AnalyseInterface::AnalysePrintFolder(string folderPath)
 
 // Constructors
 AnalyseInterface::AnalyseInterface()
+: db("DiseaseForecaster.db")
 {
-  cout << "Constructeur" << endl;
+  #ifdef DEBUG
+		cout << "Analyse constructor call" << endl;
+	#endif // DEBUG
 }
 
 // Destructors
 AnalyseInterface::~AnalyseInterface()
 {
-  cout << "Destructeur" << endl;
+  #ifdef DEBUG
+		cout << "Analyse destructor call" << endl;
+	#endif // DEBUG
 }
 
 // Private
@@ -94,7 +131,7 @@ vector<HealthPrint*> AnalyseInterface::getHealthPrintFromFile(string filePath)
     {
         if (i == 0)
         {
-          cout << *it2 << endl;
+          //cout << *it2 << endl;
           attributesTitle.push_back(*it2);
           continue;
         }
@@ -106,19 +143,18 @@ vector<HealthPrint*> AnalyseInterface::getHealthPrintFromFile(string filePath)
         strAttribute = *it2;
         try {
           numericAttribute = stod(strAttribute);
-          cout << "Insert Double : " << attributesTitle[j] << " " << j << endl;
-          cout  << " " << numericAttribute << endl;
+          //cout << "Insert Double : " << attributesTitle[j] << " " << j << endl;
+          //cout  << " " << numericAttribute << endl;
           continuousAttributesValues.insert(std::pair<string, double>(attributesTitle[j], numericAttribute));
         }
         catch ( const std::exception & e )
         {
-          cout << "Insert String : " << attributesTitle[j] << " " << j << " " << strAttribute << endl;
+          //cout << "Insert String : " << attributesTitle[j] << " " << j << " " << strAttribute << endl;
           discreteAttributesValues.insert(std::pair<string, string>(attributesTitle[j], strAttribute));
         }
 
         j++;
     }
-    cout << endl; //TODO REMOVE
     if (i > 0)
       healthPrints.push_back(new HealthPrint(i, "unnamed", "unnamed", "nodate", -1, continuousAttributesValues, discreteAttributesValues));
 
@@ -127,10 +163,23 @@ vector<HealthPrint*> AnalyseInterface::getHealthPrintFromFile(string filePath)
     i++;
   }
 
-  for (auto&& it : attributesTitle)
+  return healthPrints;
+}
+
+vector<Disease> AnalyseInterface::getAllDiseases()
+{
+  vector<Disease> diseases = db.getDiseases();
+
+  for (auto&& it : diseases)
   {
-    cout << it << endl;
+    cout << "ligne " << it.getId() << " " << it.getName() << endl;
+    vector<shared_ptr<Attribute>> attributes = db.getDiscriminantAttributesForDisease(it.getId());
+
+    for (auto&& it2 : attributes)
+    {
+      it.addDiscriminantAttribute(it2);
+    }
   }
 
-  return healthPrints;
+  return diseases;
 }
